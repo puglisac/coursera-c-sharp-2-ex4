0c9b20692a8aad5eaaeebab4fde102f0326785b8
puglisac <alan.c.puglisi@gmail.com>
2021-01-07 10:01:05 -0600

    added comments to program.cs

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -10,7 +10,7 @@ namespace Exercise4
         {
             int lower = 3;
             int upper = 9;
-
+            //print integers from 3 to 9 (inclusive)
             for(int i = lower; i <= upper; i++)
             {
                 Console.WriteLine(i);
@@ -22,16 +22,19 @@ namespace Exercise4
 
             deck.Shuffle();
 
+            //add 5 cards to the deck
             for(int i = 0; i<=4; i++)
             {
                 hand.Add(deck.TakeTopCard());
             }
 
+            //flip over each card
             for(int i = 0; i<hand.Count; i++)
             {
                 hand[i].FlipOver();
             }
 
+            //print each card
             foreach(Card card in hand)
             {
                 card.Print();
