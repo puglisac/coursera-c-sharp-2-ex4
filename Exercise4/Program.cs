17c59375d8a57620ac957c276e1f3e674af9ccdb
puglisac <alan.c.puglisi@gmail.com>
2021-01-07 09:56:20 -0600

    exercise complete

---
+++ Exercise4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleCards;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lower = 3;
+            int upper = 9;
+
+            for(int i = lower; i <= upper; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Deck deck = new Deck();
+
+            List<Card> hand = new List<Card>();
+
+            deck.Shuffle();
+
+            for(int i = 0; i<=4; i++)
+            {
+                hand.Add(deck.TakeTopCard());
+            }
+
+            for(int i = 0; i<hand.Count; i++)
+            {
+                hand[i].FlipOver();
+            }
+
+            foreach(Card card in hand)
+            {
+                card.Print();
+            }
+        }
+    }
+}
